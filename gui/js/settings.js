68c74219bad8de10f57ec485d291236261a1b22b
puru1joy <puru1joy@gmail.com>
2015-10-01 16:27:06 +0530

    theme to be added in settings settings are now loading properly

--- gui/js/settings.js
+++ gui/js/settings.js
@@ -1,114 +1,116 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-  chrome.storage.local.get({
-    theme : light,
-    proxy_type: "none",
-    proxy_address : "none",
-    proxy_port : "none",
-    autostart_tor : false,
-    accessible_ports : "all",
-    tor_port : 9999
-  }, function (items) {
-    console.log(items);
-    populate_gui(items);
-  });
+    chrome.storage.local.get({
+        theme: "light",
+        proxy_type: "none",
+        proxy_address: "none",
+        proxy_port: "none",
+        autostart_tor: false,
+        accessible_ports: "all",
+        tor_port: 9999
+    }, function (items) {
+        console.log(items);
+        populate_gui(items);
+    });
 });
 
 //populate_gui
-function populate_gui(saved_settins){
-
+function populate_gui(items) {
+    //set theme
+    if (items.theme == 'dark') {
+        document.body.classList.add('dark');
+        document.getElementById('dt').click();
+    }
+    //set proxy if not none
+    if (items.proxy_type != "none") {        
+        document.getElementById('proxy_text').innerHTML = items.proxy_type;
+        document.getElementById('proxy__address').value = items.proxy_address;
+        document.getElementById('proxy__port').value = items.proxy_port;
+    }
+    //set autostart toggle
+    if (items.autostart_tor) {
+        document.getElementById('autostart_tor').click();
+    }
+    //accessible ports
+    if (items.accessible_ports != "all") {
+        document.getElementById('acsport').click();        
+        document.getElementById('acsport__port').value = items.accessible_ports;
+    }
+    //set tor port
+    document.getElementById('cport__port').value = items.tor_port;
 }
 //this object will be passed to chrome.storage.set
 var settings = {};
 var to_be_saved = {
-  configure_proxy: false,
-  accessible_ports: false
+    configure_proxy: false,
+    accessible_ports: false
 };
 //go back to home page button
 document.getElementById('back_button').addEventListener("click", function () {
-  location.href = "index.html";
-});
-//set theme
-chrome.storage.local.get('theme', function (values) {
-  if (values.theme == 'dark') {
-    document.body.classList.add('dark');
-  }
+    location.href = "index.html";
 });
 //on load effect
 document.body.classList.add('closed');
 setTimeout(function () {
-  document.body.classList.add('open');
+    document.body.classList.add('open');
 }, 10);
-//call update settings view
-updateSettingsValues();
-//update settings view
-function updateSettingsValues() {
-  //loads saved values and reflects in the UI
-  //theme switch
-  chrome.storage.local.get('theme', function (values) {
-    if (values.theme == 'dark') {
-      //to simulate true
-      document.getElementById('dt').click();
-    }
-  });
-}
 //proxy state change event
 document.getElementById('proxy').addEventListener("click", function () {
-  var chk = this.checked;
-  if (chk) {
-    document.getElementById('proxy_card').classList.add('expand');
-    document.getElementById('proxy_card').classList.add('proxycard');
-    to_be_saved["configure_proxy"] = true;
-  }
-  else {
-    document.getElementById('proxy_card').classList.remove('expand');
-    document.getElementById('proxy_card').classList.remove('proxycard');
-    to_be_saved["configure_proxy"] = false;
-  }
+    var chk = this.checked;
+    if (chk) {
+        document.getElementById('proxy_card').classList.add('expand');
+        document.getElementById('proxy_card').classList.add('proxycard');
+        to_be_saved["configure_proxy"] = true;
+    }
+    else {
+        document.getElementById('proxy_card').classList.remove('expand');
+        document.getElementById('proxy_card').classList.remove('proxycard');
+        to_be_saved["configure_proxy"] = false;
+    }
 });
 //proxy dropdown select
 var lasttarget_proxy;
 document.getElementById('select_proxy').addEventListener("click", function (e) {
-  if (lasttarget_proxy) {
-    lasttarget_proxy.classList.remove("selected");
-  }
-  //change the target element now
-  lasttarget_proxy = e.target;
-  lasttarget_proxy.classList.add("selected");
-  //set text of drop down
-  document.getElementById('proxy_text').innerHTML = e.target.getAttribute("data-text");
+    if (lasttarget_proxy) {
+        lasttarget_proxy.classList.remove("selected");
+    }
+    //change the target element now
+    lasttarget_proxy = e.target;
+    lasttarget_proxy.classList.add("selected");
+    //set text of drop down
+    document.getElementById('proxy_text').innerHTML = e.target.getAttribute("data-text");
 });
 document.getElementById('acsport').addEventListener("click", function () {
-  var chk = this.checked;
-  if (chk) {
-    document.getElementById('acsport_card').classList.add('expand');
-    document.getElementById('acsport_card').classList.add('acsportcard');
-    to_be_saved["accessible_ports"] = true;
-  }
-  else {
-    document.getElementById('acsport_card').classList.remove('expand');
-    document.getElementById('acsport_card').classList.remove('acsportcard');
-    to_be_saved["accessible_ports"] = false;
-  }
+    var chk = this.checked;
+    if (chk) {
+        document.getElementById('acsport_card').classList.add('expand');
+        document.getElementById('acsport_card').classList.add('acsportcard');
+        to_be_saved["accessible_ports"] = true;
+    }
+    else {
+        document.getElementById('acsport_card').classList.remove('expand');
+        document.getElementById('acsport_card').classList.remove('acsportcard');
+        to_be_saved["accessible_ports"] = false;
+    }
 });
 //autostart_tor
 document.getElementById('autostart_tor').addEventListener("click", function () {
-  if (this.checked) {
-    chrome.storage.local.set({'autostart_tor': true});
-  }
-  else {
-    chrome.storage.local.set({'autostart_tor': false});
-  }
+    if (this.checked) {
+        chrome.storage.local.set({ 'autostart_tor': true });
+    }
+    else {
+        chrome.storage.local.set({ 'autostart_tor': false });
+    }
 });
 //theme switch
 document.getElementById('dt').addEventListener("click", function () {
-  if (this.checked) {
-    document.body.classList.add('dark');
-    chrome.storage.local.set({'theme': 'dark'});
-  }
-  else {
-    document.body.classList.remove('dark');
-    chrome.storage.local.set({'theme': 'light'});
-  }
+    if (this.checked) {
+        document.body.classList.add('dark');
+        chrome.storage.local.set({ 'theme': 'dark' });
+    }
+    else {
+        document.body.classList.remove('dark');
+        chrome.storage.local.set({ 'theme': 'light' });
+    }
 });
 //bridges state change event
 //document.getElementById('bridges').addEventListener("click", function () {
@@ -148,68 +150,68 @@ document.getElementById('dt').addEventListener("click", function () {
 //    }
 //});
 
-function raise_error(error,id){
-  console.log(error +  " on id " + id);
+function raise_error(error, id) {
+    console.log(error + " on id " + id);
 }
 
 function save_settings() {
-  // ------ tor port ------------
-  var tor_port = document.getElementById('cport__port').value;
-  if (0 < tor_port && tor_port <= 65535) {
-    settings["tor_port"] = tor_port;
-  } else {
-    settings["tor_port"] = 9999;
-    raise_error("incorrect tor port", 'cport__port');
-  }
-
-  // ---------- configure proxy ------------
-  if (to_be_saved["configure_proxy"]) {
-    var proxy_type = document.getElementById('proxy_text').innerHTML;
-    console.log(proxy_type);
-    if (proxy_type == "Proxy Type") {
-      raise_error("please select a proxy type or disable it", 'proxy_text');
+    // ------ tor port ------------
+    var tor_port = document.getElementById('cport__port').value;
+    if (0 < tor_port && tor_port <= 65535) {
+        settings["tor_port"] = tor_port;
     } else {
-      var proxy_port = document.getElementById('proxy__port').value;
-      if (0 < proxy_port && proxy_port <= 65535) {
-        settings["proxy_type"] = proxy_type;
-        settings["proxy_address"] = document.getElementById('proxy__address').value;
-        settings["proxy_port"] = proxy_port;
-      } else {
-        settings["proxy_type"] = "none";
-        raise_error("incorrect proxy port", 'proxy__port');
-      }
+        settings["tor_port"] = 9999;
+        raise_error("incorrect tor port", 'cport__port');
     }
-  } else {
-    settings["proxy_type"] = "none";
-  }
 
-  // ---------- accessible ports -----------
-  if (to_be_saved["accessible_ports"]) {
-    var accessible_ports = document.getElementById('acsport__port').value;
-    var reachable_addresses ;
-    var error = false;
-    var ports_list = accessible_ports.split(',');
-    for(var port = 0 ; port < ports_list.length - 1; port++){
-      if(!(0 < ports_list[port] && ports_list[port] < 65535)){
-        raise_error("incorrect port list entered, please enter number(s) between 0 and 65535 separated by ,","acsport__port");
-        error = true;
-        break;
-      }
-      console.log(ports_list[port]);
+    // ---------- configure proxy ------------
+    if (to_be_saved["configure_proxy"]) {
+        var proxy_type = document.getElementById('proxy_text').innerHTML;
+        console.log(proxy_type);
+        if (proxy_type == "Proxy Type") {
+            raise_error("please select a proxy type or disable it", 'proxy_text');
+        } else {
+            var proxy_port = document.getElementById('proxy__port').value;
+            if (0 < proxy_port && proxy_port <= 65535) {
+                settings["proxy_type"] = proxy_type;
+                settings["proxy_address"] = document.getElementById('proxy__address').value;
+                settings["proxy_port"] = proxy_port;
+            } else {
+                settings["proxy_type"] = "none";
+                raise_error("incorrect proxy port", 'proxy__port');
+            }
+        }
+    } else {
+        settings["proxy_type"] = "none";
     }
-    if(!error){
-      reachable_addresses = "*:" + accessible_ports.replace(',',",*:");
-      console.log(ports_list);
-      console.log(reachable_addresses);
-      settings["accessible_ports"] = accessible_ports;
-      settings["reachable_addresses"] = reachable_addresses;
+
+    // ---------- accessible ports -----------
+    if (to_be_saved["accessible_ports"]) {
+        var accessible_ports = document.getElementById('acsport__port').value;
+        var reachable_addresses;
+        var error = false;
+        var ports_list = accessible_ports.split(',');
+        for (var port = 0 ; port < ports_list.length - 1; port++) {
+            if (!(0 < ports_list[port] && ports_list[port] < 65535)) {
+                raise_error("incorrect port list entered, please enter number(s) between 0 and 65535 separated by ,", "acsport__port");
+                error = true;
+                break;
+            }
+            console.log(ports_list[port]);
+        }
+        if (!error) {
+            reachable_addresses = "*:" + accessible_ports.replace(',', ",*:");
+            console.log(ports_list);
+            console.log(reachable_addresses);
+            settings["accessible_ports"] = accessible_ports;
+            settings["reachable_addresses"] = reachable_addresses;
+        }
+    } else {
+        settings["reachable_addresses"] = "all";
+        settings["accessible_ports"] = "all";
     }
-  } else {
-    settings["reachable_addresses"] = "all";
-    settings["accessible_ports"] = "all";
-  }
-  console.log(to_be_saved);
-  console.log(settings);
+    console.log(to_be_saved);
+    console.log(settings);
 }
 //save settings
 document.getElementById('save').addEventListener("click", save_settings);
