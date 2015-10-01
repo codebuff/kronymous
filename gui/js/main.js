2a9ee848f0032b339b8b92a09ba35d6abe55fc24
codebuff <nonfatalist@gmail.com>
2015-10-01 14:23:44 +0530

    added check for restart and autostart

--- gui/js/main.js
+++ gui/js/main.js
@@ -1,49 +1,66 @@
 ﻿//control the main progress bar
 document.getElementById('p1').addEventListener('mdl-componentupgraded', function () {
-    this.MaterialProgress.setProgress(44);
+  this.MaterialProgress.setProgress(44);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.storage.local.get({
+    restarting_tor: false,
+    autostart_tor: false,
+  }, function (items) {
+    console.log(items);
+    if (items.restarting_tor || items.autostart_tor) {
+      lib.init(function () {
+        runTor();
+      });
+      chrome.storage.local.set({restarting_tor: false});
+      document.getElementById('start_button_text').innerHTML = "Restart Tor";
+    }
+  });
 });
 
 //start / stop tor button
 document.getElementById('start_button').addEventListener("click", function () {
-    if (document.getElementById('start_button_text').innerHTML == "Start Tor") {
-        document.getElementById('terminal').classList.add('visible');
-        lib.init(function () {
-            runTor();
-        });
-        document.getElementById('start_button_text').innerHTML = "Restart Tor";
-    } else {
-        document.querySelector('body').classList.add('hide');
-        setTimeout(function () {
-            location.href = "restart.html";
-        }, 300);
-    }
+  if (document.getElementById('start_button_text').innerHTML == "Start Tor") {
+    document.getElementById('terminal').classList.add('visible');
+    lib.init(function () {
+      runTor();
+    });
+    document.getElementById('start_button_text').innerHTML = "Restart Tor";
+  } else {
+    chrome.storage.local.set({restarting_tor: true});
+    document.querySelector('body').classList.add('hide');
+    setTimeout(function () {
+      location.href = "restart.html";
+    }, 300);
+  }
 });
 
 //open settings button
 document.getElementById('settings_button').addEventListener("click", function () {
-    document.querySelector('body').classList.add('hide');
-    setTimeout(function () {
-        location.href = "settings.html";
-    }, 300);
+  document.querySelector('body').classList.add('hide');
+  setTimeout(function () {
+    location.href = "settings.html";
+  }, 300);
 });
 
 //get theme
-chrome.storage.local.get('theme', function (value) {
-    if (value.theme == 'dark') {
-        document.body.classList.add('dark');
-    }
+chrome.storage.local.get('theme', function (values) {
+  if (values.theme == 'dark') {
+    document.body.classList.add('dark');
+  }
 });
 
 //on load effect
 document.body.classList.add('closed');
 setTimeout(function () {
-    document.body.classList.add('open');
+  document.body.classList.add('open');
 }, 10);
 
 //help and about us
 document.getElementById('help').addEventListener("click", function () {
-    document.querySelector('body').classList.add('hide');
-    setTimeout(function () {
-        window.location.assign("about.html");
-    }, 300);
+  document.querySelector('body').classList.add('hide');
+  setTimeout(function () {
+    window.location.assign("about.html");
+  }, 300);
 });
