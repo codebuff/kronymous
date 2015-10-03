7a3cfc281c67d37f5a46e51bdc51a9a8d3570ad1
codebuff <nonfatalist@gmail.com>
2015-10-03 13:35:45 +0530

    tor now loads all parameters from saved settings

--- tor.js
+++ tor.js
@@ -6,8 +6,8 @@
 
 var saved_settings;
 
-function set_settings(settings){
-  saved_settings = settings;
+function set_settings(settings) {
+    saved_settings = settings;
 }
 NaClTerm.nmf = '../tor.nmf';
 
@@ -15,33 +15,41 @@ NaClTerm.nmf = '../tor.nmf';
  #TODO(dt) check if default port is occupied
  #TODO(dt) check if(allowed) another tor process is running outside chrome. */
 function anotherTorRunning() {
-  // since chrome 34 multiple windows in apps are not allowed by default
-  // thus returning false without any checking, (for now)
-  // return NaClTerm.started;
-  return false;
+    // since chrome 34 multiple windows in apps are not allowed by default
+    // thus returning false without any checking, (for now)
+    // return NaClTerm.started;
+    return false;
 }
 
 function populateArgv() {
-  //TODO(dt) get parameters from storage once GUI is ready.
-  //tor socks proxy port
-  NaClTerm.argv = ['--SOCKSPort', saved_settings.tor_port];
-  //playing it safe for now,tor creates relays/bridges through
-  // these addresses only
-  //accessible ports
-  if (saved_settings.accessible_ports != "all") {
-    NaClTerm.argv = NaClTerm.argv.concat(['ReachableAddresses', saved_settings.reachable_addresses]);
-  }
+    //tor socks proxy port
+    NaClTerm.argv = ['--SOCKSPort', saved_settings.tor_port];
+    //tor creates relays/bridges through these addresses only
+    //accessible ports
+    if (saved_settings.accessible_ports != "all") {
+        NaClTerm.argv = NaClTerm.argv.concat(['ReachableAddresses', saved_settings.reachable_addresses]);
+    }
+    // proxy settings
+    if (saved_settings.proxy_type != "none"){
+        var proxy_type = saved_settings.proxy_type;
+        //if(proxy_type == "http") proxy_type = "HTTPProxy";
+        if(proxy_type == "http/https") proxy_type = "HTTPSProxy";
+        if(proxy_type == "socks4") proxy_type = "Socks4Proxy";
+        if(proxy_type == "socks5") proxy_type = "Socks5Proxy";
+        NaClTerm.argv = NaClTerm.argv.concat([proxy_type, saved_settings.proxy_address + ":" + saved_settings.proxy_port]);
+    }
+
 }
 
 function runTor() {
-  if (!anotherTorRunning()) {
-    populateArgv();
-    NaClTerm.init();
-  }
+    if (!anotherTorRunning()) {
+        populateArgv();
+        NaClTerm.init();
+    }
 }
 
 window.onload = function () {
-  /*lib.init(function() {
-   runTor();
-   });*/
+    /*lib.init(function() {
+     runTor();
+     });*/
 };
